2d5e0b4199812706a3bf66fa1d79bf35aea9ad5a
Jeroen Wouters <jeroen@diasporist.org>
2021-02-03 15:28:17 +0000

    select kernel

--- progs/init-jupyter.scm
+++ progs/init-jupyter.scm
@@ -4,14 +4,16 @@
       (with s (texmacs->code (stree->tree u) "SourceCode")
         (string-append  s  "\n<EOF>\n"))))
 
-(define (jupyter-launcher)
-  (if (os-mingw?)
-      "tm_jupyter.bat"
-      "tm_jupyter"))
+(define (jupyter-launcher kernel)
+  (let ((script (if (os-mingw?) "tm_jupyter.bat" "tm_jupyter") ))
+    (if (!= kernel "") (string-append script " --kernel=" kernel)
+      script)))
 
 (plugin-configure jupyter
   (:require (url-exists-in-path? "python"))
   (:require (url-exists-in-path? "tm_jupyter"))
-  (:launch ,(jupyter-launcher))
+  (:require (url-exists-in-path? "tm_kernelspecs"))
+  (:launch ,(jupyter-launcher ""))
+  (:launch "julia-1.5" ,(jupyter-launcher "julia-1.5"))
   (:serializer ,jupyter-serialize)
   (:session "Jupyter"))
